4a5c6ee6281124a0710a285d00d0e962e3136d3d
Olrik van Dijk <ovd@gmx.net>
2021-02-28 15:56:17 +0100

    installed storybook created Button component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Button from './components/Button/Button';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Hello VaderERP
+        <h2>Hello VaderERP</h2>
+        <Button variant="success">Success</Button>
       </header>
     </div>
   );
---
+++ src/components/Button/Button.stories.js
@@ -0,0 +1,15 @@
+/* eslint-disable import/no-anonymous-default-export */
+import React from 'react';
+import Button from './Button';
+
+export default {
+    title: 'Button',
+    component: Button
+}
+
+export const Primary = () => <Button variant='primary'>Primary</Button>;
+export const Secondary = () => <Button variant='secondary'>Secondary</Button>;
+export const Success = () => <Button variant='success'>Success</Button>;
+export const Warning = () => <Button variant='warning'>Warning</Button>;
+export const Danger = () => <Button variant='danger'>Danger</Button>;
+
